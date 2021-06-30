6128ad57f2c410ecccd2611cf9af82e096f60c30
BMoClay <benmorgancleveland@gmail.com>
2021-06-30 15:10:12 -0400

    twoSum hashmap solution

--- twoSum2.js
+++ twoSum2.js
@@ -9,3 +9,23 @@
 // let numbers = [2,7,11,15] 
 // let target = 9
 
+const twoSum = (numbers, target) => {
+    
+    let hashMap = {};
+    let results = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        hashMap[numbers[i]] = i
+        console.log(`hashmap create1 ${hashMap[numbers[i]]}`)
+     if (hashMap[target - numbers[i]] !== undefined) {
+         results.push(hashMap[target - numbers[i]], hashMap[numbers[i]])
+        }
+    }
+    console.log(hashMap)
+    console.log(results)
+
+}
+
+let numbers = [5,2,11,15,7] 
+let target = 26
+twoSum(numbers,target)

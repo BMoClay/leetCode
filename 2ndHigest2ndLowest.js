2e063fe53c00d5e06e0c3a9b5d1425e2f817a093
BMoClay <benmorgancleveland@gmail.com>
2021-06-30 13:56:31 -0400

    2ndHighest2ndLowest complete

--- 2ndHigest2ndLowest.js
+++ 2ndHigest2ndLowest.js
@@ -2,7 +2,22 @@
 // // I.e. - [10,5,7,2,4,1,24] & [8,23,29,25,40,0,24] -> 2nd Largest : 29 , 2nd Smallest: 1
 
 const secondBiggestAndSmallest = () => {
-    console.log(array1, array2)
+   let combinedArray = array1.concat(array2)
+   let biggest = 0
+   let smallest = 0
+
+   for (let number of combinedArray) {
+       if (number >= biggest) {
+           biggest = number
+       }
+       if (number <= smallest) {
+           smallest = number
+       }
+   }
+
+   let sortedArray = combinedArray.sort((a,b) => a - b)
+
+   console.log(sortedArray[1], sortedArray[sortedArray.length - 2])
 }
 
 const array1 =[10,5,7,2,4,1,24]
--- twoSum.js
+++ twoSum.js
@@ -107,39 +107,41 @@
 // const target = 9
 // twoSum(nums, target)
 
-array = [2,7,11,15]
-goal = 9
+// array = [2,7,11,15]
+// goal = 9
 
-const twoSum = () => {
-    let numberMap = new Map();
+// const twoSum = () => {
+//     let numberMap = new Map();
 
-    for (let i = 0; i < array.length; i++) {
-        el = array[i]
+//     for (let i = 0; i < array.length; i++) {
+//         el = array[i]
 
-        if(numberMap.has(goal - el))
-            return [i, numberMap.get(goal - el)];
-        else numberMap.set(el, i)
-    }
-    console.log(goal)
-    return [];
-};
+//         if(numberMap.has(goal - el))
+//             return [i, numberMap.get(goal - el)];
+//         else numberMap.set(el, i)
+//     }
+//     console.log(goal)
+//     return [];
+// };
 
-twoSum(array, goal);
- array = [2,7,11,15]
-goal = 9
+// twoSum(array, goal);
+//  array = [2,7,11,15]
+// goal = 9
 
-const twoSum = () => {
-    let numberMap = new Map();
+// const twoSum = () => {
+//     let numberMap = new Map();
 
-    for (let i = 0; i < array.length; i++) {
-        el = array[i]
+//     for (let i = 0; i < array.length; i++) {
+//         el = array[i]
         
-        if(numberMap.has(goal - el))
-            return [i, numberMap.get(goal - el)];
-        else numberMap.set(el, i)
-    }
-    console.log(goal)
-    return [];
-};
+//         if(numberMap.has(goal - el))
+//             return [i, numberMap.get(goal - el)];
+//         else numberMap.set(el, i)
+//     }
+//     console.log(goal)
+//     return [];
+// };
 
-twoSum(array, goal);  
+// twoSum(array, goal);  
+
+

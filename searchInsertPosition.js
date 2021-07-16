43bce2b70910ed0acbfee0707b537ae036540d43
BMoClay <benmorgancleveland@gmail.com>
2021-07-16 18:03:43 -0400

    fix bug

--- searchInsertPosition.js
+++ searchInsertPosition.js
@@ -1,22 +1,23 @@
 const searchInsert = (nums, target) => {
     let numsObj = {};
-    let result
+    let result;
     
     for (let i = 0; i < nums.length; i++) {
         numsObj[nums[i]] = i
-        if (numsObj[target] === numsObj[nums[i]]){
+        if(numsObj[target] === numsObj[nums[i]]) {
             result = numsObj[target]
-        } else if (target > nums[i] && target < nums[i + 1]) {
-           result = i + 1
+        } else if (target > nums[i]) {
+            result = i + 1
         } else if (target > nums[i + 1]) {
             result = i + 2
+        } else if (result === undefined) {
+            result = 0
         }
     }
-    console.log(result)
     return result;
-}
+};
 
 const nums = [1,3,5,6]
-const target = 7
-searchInsert(nums, target)
-// console.log(searchInsert(nums, target))
+const target = 8
+//searchInsert(nums, target)
+console.log(searchInsert(nums, target))
